91e25e8fedbc494df7ad858e5e237df6c0b6d209
AirOne01 <popgthyrd@gmail.com>
2022-04-11 17:57:07 +0200

    Added fucking blur finally

--- renderer/components/GridLibrary.tsx
+++ renderer/components/GridLibrary.tsx
@@ -8,7 +8,10 @@ function GridLibrary (): React.ReactElement {
   return (
     <div className='game-grid p-4'>
       {list.map(e =>
-        <GridItem key={uuidv4()}>lorem ipsum</GridItem>
+        <GridItem key={uuidv4()}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, ad error aut reprehenderit vitae fugit deserunt dignissimos eius sit. Deleniti consequuntur dolorem aperiam eveniet rem dolore tenetur voluptatem necessitatibus in.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, facere dolorum. Harum culpa numquam vitae sapiente minus eligendi ducimus odit repellendus, magnam doloribus architecto ipsum, dolores incidunt sit cum ad!
+        </GridItem>
       )}
     </div>
   )
--- renderer/components/library/GridItem.tsx
+++ renderer/components/library/GridItem.tsx
@@ -7,8 +7,10 @@ class GridItem extends React.Component {
 
   render () {
     return (
-      <div className='game-item bg-red-500 rounded-md'>
-        <div className='bg-gray-600 rounded-md'>
+      <div className='game-item relative'>
+        <div className='absolute top-0 left-0 bg-red-500 rounded-lg w-full h-full filter blur'>
+        </div>
+        <div className='absolute top-0 left-0 bg-transparent rounded-lg w-full h-full p-2'>
           {this.props.children}
         </div>
       </div>
