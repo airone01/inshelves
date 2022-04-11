13f9b2bd2d9af348b7ac5a3014adf2a0cffacc25
AirOne <popGthyrd@gmail.com>
2022-04-12 00:44:36 +0200

    Add hover and glowing effects on game grid and sidebar

--- renderer/components/library/GridItem.tsx
+++ renderer/components/library/GridItem.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 class GridItem extends React.Component {
+  private color: string;
+
   constructor (props) {
     super(props);
+    this.color = props.color;
   }
 
   render () {
     return (
-      <div className='game-item relative'>
-        <div className='absolute top-0 left-0 bg-red-500 rounded-lg w-full h-full filter blur'>
-        </div>
-        <div className='absolute top-0 left-0 bg-transparent rounded-lg w-full h-full p-2'>
+      <div className='game-item relative group'>
+        <div className='absolute w-full h-full top-4 left-4 rounded-lg transition-transform duration-400 filter blur-sm' style={{ backgroundColor: this.color ? this.color : 'black' }} />
+        <div className='absolute w-full h-full top-4 left-4 rounded-lg transition-transform duration-400 p-2 bg-gray-700'>
           {this.props.children}
         </div>
       </div>
--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -1,14 +1,13 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
   content: [
     './pages/**/*.{js,jsx,ts,tsx,vue}',
     './components/**/*.{js,jsx,ts,tsx,vue}',
-  ]
+  ],
 };
