5faeb3545e83a12fb224d98a2a374c7351fdac4d
AirOne01 <popgthyrd@gmail.com>
2022-06-19 21:00:05 +0200

    feat(lint): add Prettier and ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['src-tauri/target/**/*.js', 'src-tauri/WixTools/**/*.js'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+}
